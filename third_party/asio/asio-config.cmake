6f1bd2b76f679868aae39da08f7f684ff9b2a427
Alexej Harm <aharm@xdsnet.de>
2016-06-06 16:41:32 +0200

    added libressl and asio_ssl

--- third_party/asio/asio-config.cmake
+++ third_party/asio/asio-config.cmake
@@ -5,11 +5,20 @@ get_filename_component(ASIO_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
 if(NOT TARGET asio)
   add_library(asio INTERFACE IMPORTED)
   set_target_properties(asio PROPERTIES
-    INTERFACE_COMPILE_DEFINITIONS "ASIO_STANDALONE=1"
-    INTERFACE_INCLUDE_DIRECTORIES "${ASIO_PATH}/include")
+    INTERFACE_INCLUDE_DIRECTORIES "${ASIO_PATH}/include"
+    INTERFACE_COMPILE_DEFINITIONS "ASIO_STANDALONE=1")
   if(MSVC)
     set_target_properties(asio PROPERTIES
       INTERFACE_COMPILE_DEFINITIONS "ASIO_STANDALONE=1;ASIO_MSVC=1"
       INTERFACE_LINK_LIBRARIES "comctl32.lib;ws2_32.lib")
   endif()
+endif()
+
+find_package(libressl PATHS "${ASIO_PATH}/../libressl")
+
+if(LIBRESSL_FOUND AND NOT TARGET asio_ssl)
+  add_library(asio_ssl INTERFACE IMPORTED)
+  set_target_properties(asio_ssl PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${ASIO_PATH}/include"
+    INTERFACE_LINK_LIBRARIES "asio;ssl")
 endif()
